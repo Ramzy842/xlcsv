a45fe9ff017bc6c27e21777b82d04e60264a715f
Ramzy842 <ramzychahbani@gmail.com>
2024-06-19 09:39:01 +0100

    changes

--- src/app/Components/Uploader.js
+++ src/app/Components/Uploader.js
@@ -17,16 +17,31 @@ const Uploader = ({ setFiles }) => {
     const [fileName, setFileName] = useState("");
     const [fileSize, setFileSize] = useState("");
     const [filePresent, setFilePresent] = useState(false);
+    const [uploadedFiles, setUploadedFiles] = useState([]);
+    // const delay = (ms) => new Promise((res) => setTimeout(res, ms));
 
-    const delay = (ms) => new Promise((res) => setTimeout(res, ms));
-    async function handler(e) {
-        const input = document.querySelector("input");
+    function handleUploadedFiles(e) {
         e.stopPropagation();
         e.preventDefault();
-        let kinds = { folders: 0, files: 0 };
-        if (input.files[0].type === "application/vnd.ms-excel") {
-            for (let x = 0; x < input.files.length; x++) {
-                input.files[x].arrayBuffer().then((res) => {
+        setUploadedFiles(e.dataTransfer.files);
+        if (uploadedFiles && uploadedFiles[0]) handleInput();
+    }
+
+    useEffect(() => {
+        if (uploadedFiles.length && uploadedFiles[0]) handleInput();
+    }, [uploadedFiles]);
+
+    function handler(e) {
+        e.stopPropagation();
+        e.preventDefault();
+
+        for (let x = 0; x < uploadedFiles.length; x++) {
+            const isCorrectFileExt =
+                uploadedFiles[x].type === "application/vnd.ms-excel" ||
+                uploadedFiles[x].type ===
+                    "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
+            if (uploadedFiles[x].type && isCorrectFileExt) {
+                uploadedFiles[x].arrayBuffer().then((res) => {
                     let data = new Uint8Array(res);
                     let wb = XLSX.read(data, { type: "array" });
                     const first_sheet_name = wb.SheetNames[x];
@@ -36,18 +51,17 @@ const Uploader = ({ setFiles }) => {
                     });
                     let newWorkSheet = XLSX.utils.json_to_sheet(jsonData);
                     let new_wb = XLSX.utils.book_new();
-                    const fileNameWithoutExt = fileName.substring(
-                        0,
-                        fileName.lastIndexOf(".")
-                    );
-                    XLSX.utils.book_append_sheet(
-                        new_wb,
-                        newWorkSheet,
-                        fileNameWithoutExt
-                    );
-					setFilePresent(true);
-                    delay(1000).then(() => {
-                        
+                    if (uploadedFiles[x]) {
+                        const temp_name = uploadedFiles[x].name;
+                        const fileNameWithoutExt = uploadedFiles[
+                            x
+                        ].name.substring(0, temp_name.lastIndexOf("."));
+                        XLSX.utils.book_append_sheet(
+                            new_wb,
+                            newWorkSheet,
+                            fileNameWithoutExt
+                        );
+                        setFilePresent(true);
                         setFiles((prev) => [
                             ...prev,
                             {
@@ -58,26 +72,24 @@ const Uploader = ({ setFiles }) => {
                                 },
                             },
                         ]);
-                    });
+                    }
                 });
-                if (!input.files[x].type)
-                    kinds = { ...kinds, folders: kinds.folders + 1 };
-                else kinds = { ...kinds, files: kinds.files + 1 };
             }
-            if (kinds.folders == 1) console.log("You dropped 1 folder.");
-            else if (kinds.files == 1) console.log("You dropped 1 file.");
-            else if (kinds.folders > 1)
-                console.log(`You dropped ${kinds.folders} folders!`);
-            else if (kinds.files > 1)
-                console.log(`You dropped ${kinds.files} files!`);
         }
     }
 
-    function handleInput(e) {
-        setFileName(e.target.files[0].name);
-        setFileSize(convertBytes(e.target.files[0].size));
+    function handleInput() {
+        setFileName(uploadedFiles[uploadedFiles.length - 1].name);
+        setFileSize(convertBytes(uploadedFiles[uploadedFiles.length - 1].size));
     }
 
+    function handleRefresh() {
+        setFileName("");
+        setFileSize("");
+        setFilePresent(false);
+        setUploadedFiles([]);
+        setFiles([]);
+    }
     function convertBytes(value) {
         const units = {
             KB: 1024,
@@ -98,12 +110,22 @@ const Uploader = ({ setFiles }) => {
     return (
         <div
             id="upload-zone"
-            className={`w-56 flex flex-col items-center justify-center mx-auto mt-16 bg-black rounded-md p-4 drop-shadow-2xl max-w-xl ${fileName && !filePresent && "w-auto  border-b-2 border-yellow-400 "} ${filePresent && "border-b-2 w-auto border-green-500"}`}
+            className={`w-56 flex flex-col items-center justify-center mx-auto mt-16 bg-black/70 backdrop-blur-xl rounded-md p-4 drop-shadow-2xl max-w-lg ${
+                fileName &&
+                !filePresent &&
+                "w-auto  border-b-4 border-yellow-500 "
+            } ${filePresent && "border-b-4 w-auto border-green-500"}`}
         >
-           {/*  {!fileName && (
-                <>
-                    <p className="font-semibold mb-4 text-white">
-                        Drop Your Folder Here
+            {!uploadedFiles.length && (
+                <div
+                    className="flex flex-col items-center"
+                    onDragEnter={handleUploadedFiles}
+                    onDragOver={handleUploadedFiles}
+                    onDrop={handleUploadedFiles}
+                    onDragLeave={handleUploadedFiles}
+                >
+                    <p className="font-semibold mb-4 text-white ">
+                        Drop Your Files Here
                     </p>
                     <Image
                         src={`./assets/upload.svg`}
@@ -112,8 +134,8 @@ const Uploader = ({ setFiles }) => {
                         className="cursor-pointer"
                         alt="upload"
                     />
-                </>
-            )} */}
+                </div>
+            )}
             {fileName && (
                 <div className="flex text-white justify-between w-full  ">
                     <div className="w-3/5 max-w-md">
@@ -153,14 +175,24 @@ const Uploader = ({ setFiles }) => {
                     )}
                 </div>
             )}
-            {
-                <input
-                    accept=".xls, .xlsx"
-                    className={`${fileName && "hidden"} text-white`}
-                    type="file"
-                    onChange={handleInput}
+
+            {/* <input
+                accept=".xls, .xlsx"
+                className={`${fileName && "hidden"} text-white`}
+                type="file"
+                onChange={handleInput}
+            /> */}
+
+            {fileName && (
+                <Image
+                    src={"./assets/refresh.svg"}
+                    className="cursor-pointer"
+                    width={20}
+                    height={20}
+                    alt="refresh"
+                    onClick={handleRefresh}
                 />
-            }
+            )}
         </div>
     );
 };
