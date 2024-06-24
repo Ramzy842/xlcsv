13be3370f2b1d10b6e2c67af3ed6a818811e3dce
Ramzy842 <ramzychahbani@gmail.com>
2024-06-25 00:37:07 +0100

    modif

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    basePath: "/xlcsv",
-    output: "export", // <=== enables static exports
-    reactStrictMode: true,
+  
 };
 
 export default nextConfig;
